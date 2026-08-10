36522d5b821bcb9fca001d5b41c51666e6bb4ca4
Mahmoud Wael <arnaout.4040@gmail.com>
2026-08-11 01:36:14 +0300

    Add user creation schema

--- app/api/users.py
+++ app/api/users.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter
 
+from app.schemas.user import UserCreate
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post("/")
+def create_user(user: UserCreate):
+    return {
+        "message": "User received successfully",
+        "user": user,
+    }
 
 
 @router.get("/")
