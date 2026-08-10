468b3cbb2a7a2a26ef5e5171ed33b72cae1c8568
Mahmoud Wael <arnaout.4040@gmail.com>
2026-08-11 01:44:07 +0300

    Connect users API to PostgreSQL

--- app/api/users.py
+++ app/api/users.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from app.core.database import get_db
+from app.models.user import User
 from app.schemas.user import UserCreate
 
 
@@ -7,15 +10,19 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/")
-def create_user(user: UserCreate):
-    return {
-        "message": "User received successfully",
-        "user": user,
-    }
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        email=user.email,
+        name=user.name,
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
 
 @router.get("/")
-def get_users():
-    return {
-        "message": "Users endpoint is working"
-    }
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
